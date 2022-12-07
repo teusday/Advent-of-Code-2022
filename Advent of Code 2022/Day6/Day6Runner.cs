0ab6e15ae5931f9e5df2eb6335eaaab185fb028c
Zak Sharp <macmaster195@gmail.com>
2022-12-06 21:34:58 -0600

    Day 6 part B

--- Advent of Code 2022/Day6/Day6Runner.cs
+++ Advent of Code 2022/Day6/Day6Runner.cs
@@ -30,9 +30,34 @@ namespace Advent_of_Code_2022.Day6
             }
 
             if (packetStart == null)
+            {
                 Console.WriteLine("Could not locate signal start");
+                return;
+            }
             else
                 Console.WriteLine($"Signal start at character {packetStart + 1}");
+
+            int? messageStart = null;
+            var searchStart = Math.Max(packetStart.Value, 13);
+            for (int i = searchStart; i < signal.Length; i++)
+            {
+                var test = new ArraySegment<char>(signal, i - 13, 14);
+                var allDistinct = test.Distinct().Count() == 14;
+
+                if (allDistinct)
+                {
+                    messageStart = i;
+                    break;
+                }
+            }
+
+            if (messageStart == null)
+            {
+                Console.WriteLine("Could not locate message start");
+                return;
+            }
+            else
+                Console.WriteLine($"Message start at character {messageStart + 1}");
         }
     }
 }
